6be3712cefdf888c42cfa7a4fc5f67bde3eefa16
haitao <375531039@qq.com>
2017-04-14 16:57:25 +0800

    添加GSM短信发送功能，key2开启和关闭

--- CoordinatorRCT6/USER/main.c
+++ CoordinatorRCT6/USER/main.c
@@ -35,6 +35,7 @@ char* toHex(u8 num)
 
 char  hexString[40] = {0};
 
+//字节转换成16进制
 char * bytesToHex(u8 cs[], int len)
 {
 	int i=0, id=0, num;
@@ -48,6 +49,7 @@ char * bytesToHex(u8 cs[], int len)
 	return hexString;
 }
 
+//清空数组
 void clean_chars(char c[], int len)
 {
 	int i=0;
@@ -63,9 +65,11 @@ u8 te[] = {0x4f,0x4b,'g'};
 
 
 void keyFun(void);
-int dir = 1;
-int angle = 90;
+int dir = 1; //电机转动方向控制
+int angle = 90;//电机转动角度控制
 int open_lock = 0; //车位锁是否开启
+
+u8 gsm_flag = 1; //是否要发送短信，1 发送， 0不发送
 
  int main(void)
  {	
@@ -75,65 +79,66 @@ int open_lock = 0; //车位锁是否开启
 	delay_init();	    	 //延时函数初始化	
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);// 设置中断优先级分组2
 	uart_init(9600);	 //串口初始化为9600
-	USART2_Init(9600);
+	USART2_Init(9600);//串口2连接GPRS模块
 	LED_Init();		  	 //初始化与LED连接的硬件接口 
 	KEY_Init();
 	TIM2_Int_Init(99,7199);//10Khz的计数频率，计数99为10ms 
 	
-	motor_init();
-	capture_init();
+	motor_init();//电机初始化
+	capture_init();//超声波测距初始化
 	
 	LCD_Init() ;
 	
-	toHex(userId[0]);
+	toHex(userId[0]);//显示停车场编号，转换为16进制显示
 	park_num[0] =  hexRes[0];
 	park_num[1] =  hexRes[1];
 	toHex(userId[1]);
 	park_num[2] =  hexRes[0];
 	park_num[3] =  hexRes[1];
-	
+	//显示停车场编号，转换为16进制显示
 	sprintf(park_num, "park:%c%c%c%c", park_num[0],park_num[1],park_num[2],park_num[3]);
-	LCD_P8x16Str(0,0,park_num);
+	LCD_P8x16Str(0,0,park_num);//显示停车场编号，转换为16进制显示
 	 
-	LED2=1;
+	
 	LED3=0;
 	
 	USART1_send_data(te,2);//好像串口第一次发送时候第一个字节会没有，故初始化先发送一次
 	USART2_send_data(te,2);
 	
-	connectServer();
+	connectServer();//GPRS模块连接服务器
 	
 	while(1)
 	{
-		keyFun();		
+		keyFun();	
+		LED2=gsm_flag;//表明是否开启短信发送功能，亮 开启，灭 关闭，由key2控制
 	}	 
 }
 
 //0D 0A 43 4F 4E 4E 45 43 54 20 4F 4B 0D 0A
 
-u8 ok[6] = {0x0D,0x0A,0x4F,0x4B,0x0D,0x0A};
-volatile u8 connectRequestFlag = 0;
-volatile u8 connectOkFlag = 0, sendInfoFlag = 0;
-volatile u8 isSending = 0; 
+u8 ok[6] = {0x0D,0x0A,0x4F,0x4B,0x0D,0x0A};//AT指令返回的成功字符串
+volatile u8 connectRequestFlag = 0; //连接请求标志位
+volatile u8 connectOkFlag = 0, sendInfoFlag = 0; //连接成功标志位，发送信息标志位
+volatile u8 isSending = 0; //正在发送标志位
 
 						   
 u8 connectOk[] = {0x0D, 0x0A, 0x4F, 0x4B, 0x0D, 0x0A,
 				  0x0D, 0x0A, 0x43, 0x4F,
 				  0x4E, 0x4E, 0x45, 0x43, 
-				  0x54, 0x20, 0x4F, 0x4B, 0x0D, 0x0A};
+				  0x54, 0x20, 0x4F, 0x4B, 0x0D, 0x0A};//AT指令返回连接成功
 
 u8 alreadyCon[] = {0x0D, 0x0A, 0x45, 0x52,
 				   0x52, 0x4F, 0x52, 0x0D, 
 				   0x0A, 0x0D, 0x0A, 0x41, 
-				   0x4C, 0x52, 0x45, 0x41};
+				   0x4C, 0x52, 0x45, 0x41};//AT指令返回已经连接
 
-u8 sendOK[4] = {0x0D, 0x0A, 0x53, 0x45};
+u8 sendOK[4] = {0x0D, 0x0A, 0x53, 0x45};//AT指令返回发送成功
 
 u8 connectIndex = 0;
 u8 sendIndex = 0;
 u8 recSIM[30];
 
-u8 isConnect(u8 rec[]) 
+u8 isConnect(u8 rec[]) //判断GPRS是否连接成功
 {
 	u8 i = 0;
 	for (i=0; i<16; i++)
@@ -146,7 +151,7 @@ u8 isConnect(u8 rec[])
 	return 1;
 }
 
-u8 isAlreadyConnect(u8 rec[]) 
+u8 isAlreadyConnect(u8 rec[]) //判断GPRS是否已经连接成功
 {
 	u8 i = 0;
 	for (i=0; i<16; i++)
@@ -159,7 +164,7 @@ u8 isAlreadyConnect(u8 rec[])
 	return 1;
 }
 
-u8 isSendOK() 
+u8 isSendOK() //判断是否发送数据成功  GPRS
 {
 	u8 i = 0,sendRec[4] = {0};
 	for (i=0; i<4; i++)
@@ -190,7 +195,7 @@ u8 isSendOK()
 
 
 
- //显示发送状态
+ //显示发送状态 LCD
 void showLcdStr1(u8 * data)
 {
 	LCD_P8x16Str(0,2,"              ");
@@ -211,7 +216,7 @@ void showLcdStr3(u8 * data)
 	LCD_P8x16Str(0,6,data);	
 }
 
-void registUser()
+void registUser() //GPRS 服务器注册ＩＤ
 {
 	showLcdStr1("regist     ");								
 	printf("AT+CIPSEND\r\n");
@@ -222,7 +227,7 @@ void registUser()
 	//isSendOK();
 }
 
-void sendData(u8 * data)
+void sendData(u8 * data)//向GPRS 服务器发送数据
 {
 	showLcdStr1("sending");								
 	printf("AT+CIPSEND\r\n");
@@ -234,7 +239,7 @@ void sendData(u8 * data)
 	isSendOK();
 }
 
-void sendData2(u8 data[], int len)
+void sendData2(u8 data[], int len)//向GPRS 服务器发送数据
 {
 	showLcdStr1("sending");								
 	printf("AT+CIPSEND\r\n");
@@ -247,21 +252,21 @@ void sendData2(u8 data[], int len)
 	isSendOK();
 }
 
-void sendDataBefor() 
+void sendDataBefor() //向GPRS 服务器发送数据前的指令，没有用
 {
 	showLcdStr1("sending");								
 	printf("AT+CIPSEND\r\n");
 	delay_ms(500);
 }
 
-void sendDataEnd() 
+void sendDataEnd() //没有用到
 {
 	printf("%c\r\n",0x1A);
 	isSendOK();
 }
 
 
-void connectServer() 
+void connectServer() //连接GPRS服务器
 {	
 	int time_us = 300;
 
@@ -313,12 +318,39 @@ void connectServer()
 		
 }
 
+void send_gsm()//发送GSM短信提醒  中文短信
+{
+	int time_us = 300;
+	printf("AT+CMGF=1\r\n");
+	delay_ms(time_us);
+	
+	printf("AT+CSMP=17,167,2,25\r\n");
+	delay_ms(time_us);
+	
+	printf("AT+CSCS=\"UCS2\"\r\n");
+	delay_ms(time_us);
+	
+	printf("AT+CMGF=1\r\n");
+	delay_ms(time_us);
+	
+	//设定手机号码 需要转换为Unicode码
+	printf("AT+CMGS=\"00310037003700320039003800340039003300370032\"\r\n");
+	delay_ms(300);
+	
+	printf("5C0A656C76847528623760A8597DFF0C60A876848F668F865DF27ECF79BB5F00505C8F664F4DFF0C8BF753CA65F67ED37B97672C6B21505C8F668D397528FF0C6B228FCE60A876844E0B4E006B214F7F7528FF0C8BF76CE8610F5B895168FF01\r\n");
+	delay_ms(300);
+	
+	printf("%c",0x1A);
+	delay_ms(300);
+	
+}
+
 void keep_connect(void)
 {
 	
 }
 
-void closeServer() 
+void closeServer() //关闭服务器连接
 {
 	int time_us = 1000;
 	printf("AT+CIPCLOSE=1\r\n");
@@ -335,7 +367,7 @@ void closeServer()
 
 u8 fb_all_state[30] = {0x73,0x2d,0x5B};//以's-' 开头，后面依次是每个停车位的状态，从001开始，1表示已使用，0表示未使用
 
-unsigned char rec_cmd[4] = {0x5A,0x00,0x02,0x51};
+unsigned char rec_cmd[4] = {0x5A,0x00,0x02,0x51};//接收到的车辆离开的反馈指令 第一个字节头 中间2个表示车位编号，最后一个表示车辆离开命令
 u8 cmd_string[20] = {0};
 int cmd_times = 0;
 
@@ -346,7 +378,7 @@ char rec_index = 1;
 char table_state[CAR_PARK_NUM+1] = {0}; //默认有CAR_PARK_NUM个停车位，/
                                         //未用为0，已使用为1，对应下标停车位编号
 
-void USART1_IRQHandler(void)                	//串口1中断服务程序
+void USART1_IRQHandler(void)                	//串口1中断服务程序，连接GPRS模块
 {
 	static u8 recCommandFlag = 0;
 	u8 res;
@@ -443,7 +475,8 @@ void USART1_IRQHandler(void)                	//串口1中断服务程序
 u8 fb_cmd[] = {0x73,0x2d,0x5a,0x00,0x01,0x53};//以's-' 开头，很奇怪，自动发送时候第一个 s 字符无法发送出去
 
 
-void USART2_IRQHandler(void)    
+
+void USART2_IRQHandler(void)    //串口2中断  连接ZIgBee模块
 {  
     u8 res;
 	static int fb_times = 0;
@@ -473,6 +506,10 @@ void USART2_IRQHandler(void)
 				//很奇怪，自动发送时候第一个 s 字符无法发送出去
 				sendData2(fb_cmd, 6);
 				
+				delay_ms(500);
+				//发送短信提醒
+				if (gsm_flag == 1)
+					send_gsm();
 				
 				//状态表更新
 				table_state[rec_cmd[2]] = 0; //该停车位被释放
@@ -531,8 +568,9 @@ void keyFun(void)
 			connectServer();
 		
 			break;
-		case KEY2_PRES:
-			keep_connect();
+		case KEY2_PRES://切换短信发送开关
+			//send_gsm();
+			gsm_flag = !gsm_flag;
 			break;
 		case KEY3_PRES:	
 			
